26ed3d333ce39f909b99f1e90cd42e0ffeb54348
supriya759694 <supriyabhowmick321@gmail.com>
2026-06-24 13:14:05 +0530

    Add Streamlit integration and movie selection feature with pickle data

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies_list = movies_list['title'].values
+
+
 st.title('Movie Recommender System')
 
+selected_movie_name = st.selectbox(
+    'Select a movie:',
+    movies_list
+)
+
+if st.button('Recommend'):
+    st.write('You selected:' , selected_movie_name)
+    
